27c0d9da0ae7548dc427ba14b56912f6a3bfa4d7
RameshIOS <ramesh.guddala@gmail.com>
2025-03-15 03:16:29 -0400

    Initial Commit

---
+++ LeaderBoard/LeaderBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderBoardApp.swift
+//  LeaderBoard
+//
+//  Created by Ramesh Guddala on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LeaderBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
